9c1ef18b9ad7d8f5c116ac02deb96018182cd546
yeisson12 <113949437+yeisson12@users.noreply.github.com>
2023-11-29 13:00:00 -0500

    actualizacion sliders

--- js/index.js
+++ js/index.js
@@ -27,7 +27,7 @@ window.addEventListener('DOMContentLoaded', () => {
         const [catalogoUno, catalogoDos, catalogoTres] = catologos;
 
         // Catalogo uno
-        const populares = document.getElementById('populares');
+        const populares = document.querySelector('#populares');
         catalogoUno.results.forEach(pelicula => {
             const article = document.createElement('article');
             article.classList.add('pelicula');
@@ -38,7 +38,7 @@ window.addEventListener('DOMContentLoaded', () => {
         });
 
         // Catalogo dos
-        const estrenos = document.getElementById('estreno');
+        const estrenos = document.querySelector('#estreno');
         catalogoDos.results.forEach(pelicula => {
             const article = document.createElement('article');
             article.classList.add('pelicula');
@@ -49,7 +49,7 @@ window.addEventListener('DOMContentLoaded', () => {
         });
 
         // Catalogo tres
-        const vistas = document.getElementById('vistas');
+        const vistas = document.querySelector('#vistas');
         catalogoTres.results.forEach(pelicula => {
             const article = document.createElement('article');
             article.classList.add('pelicula');
@@ -58,73 +58,51 @@ window.addEventListener('DOMContentLoaded', () => {
             article.append(img);
             vistas.append(article);
         });
-        
+
+        // Initialize sliders
+        initializeSlider('populares');
+        initializeSlider('estreno');
+        initializeSlider('vistas');
+        initializeSlider('top');
     });
 });
 
+function initializeSlider(sliderId) {
+    const filaPelicula = document.querySelectorAll(`#${sliderId} .peliculas-top`);
+    const fila = document.querySelector(`#${sliderId} .peliculas`);
+    const peliculas = document.querySelector(`#${sliderId}`);
+    
+    const flechaIzquierda = document.querySelector(`#${sliderId} .topLeft`);
+    const flechaDerecha = document.querySelector(`#${sliderId} .topRigth`);
 
-const filaPelicula = document.querySelectorAll(".peliculas-top")
-const fila = document.querySelector(".peliculas");
-const peliculas = document.querySelector(".slide-dos");
+    // Rest of your slider code...
 
-const flechaIzquierda = document.querySelector(".topLeft");
-const flechaDerecha = document.querySelector('.topRigth');
+    /* paginación */
+    const numeroPagina = Math.ceil(filaPelicula.length / 5);
+    for (let i = 0; i < numeroPagina; i++) {
+        const indicador = document.createElement('button');
 
-/* Evento Flecha derecha */
+        if (i === 0) {
+            indicador.classList.add("activo");
+        }
 
-flechaDerecha.addEventListener('click', () => {
+        document.querySelector(`#${sliderId} .indicadores`).appendChild(indicador);
+        indicador.addEventListener("click", (e) => {
+            peliculas.scrollLeft = i * peliculas.offsetWidth;
 
-    peliculas.scrollLeft += peliculas.offsetWidth;
-
-    const indicadorActivo = document.querySelector(".indicadores .activo")
-
-    if (indicadorActivo.nextSibling) {
-        indicadorActivo.nextSibling.classList.add("activo");
-        indicadorActivo.classList.remove("activo");
-    }
-});
-
-/* Evento Flecha derecha */
-
-flechaIzquierda.addEventListener('click', () => {
-
-    peliculas.scrollLeft -= peliculas.offsetWidth;
-
-    const indicadorActivo = document.querySelector(".indicadores .activo")
-
-    if (indicadorActivo.previousSibling) {
-        indicadorActivo.previousSibling.classList.add("activo");
-        indicadorActivo.classList.remove("activo");
-    }
-});
-
-/* paginación */
-
-const numeroPagina = Math.ceil(filaPelicula.length / 5);
-for (let i = 0; i < numeroPagina; i++) {
-    const indicador = document.createElement('button');
-
-    if (i === 0) {
-        indicador.classList.add("activo");
+            document.querySelector(`#${sliderId} .indicadores .activo`).classList.remove("activo")
+            e.target.classList.add("activo")
+        })
     }
 
-    document.querySelector('.indicadores').appendChild(indicador);
-    indicador.addEventListener("click", (e) => {
-        peliculas.scrollLeft = i * peliculas.offsetWidth;
-
-        document.querySelector(".indicadores .activo").classList.remove("activo")
-        e.target.classList.add("activo")
-    })
+    /* Hover */
+    Array.from(peliculas.children).forEach((pelicula) => {
+        pelicula.addEventListener("mouseenter", (e) => {
+            const elemento = e.currentTarget;
+            setTimeout(() => {
+                Array.from(peliculas.children).forEach((pelicula) => pelicula.classList.remove("hover"));
+                elemento.classList.add("hover");
+            }, 300)
+        })
+    });
 }
-
-/* Hover */
-
-peliculas.forEach((peliculas) => {
-    peliculas.addEventListener("mouseenter", (e) => {
-        const elemento = e.currentTarget;
-        setTimeout(() => {
-            peliculas.forEach(peliculas => peliculas.classList.remove("hover"));
-            elemento.classList.add("hover")
-        }, 300)
-    })
-}) 
